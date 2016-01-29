47790dff8eaca145dc4a4a9318b99cb5fecf14df
Abigail Watson <awatson1978@gmail.com>
2016-01-29 11:28:22 -0800

    added gagarin tests

--- tests/gagarin/StatsCounterTests.js
+++ tests/gagarin/StatsCounterTests.js
@@ -1,30 +1,29 @@
-// 
-// describe('clinical:graphs-dailystats', function () {
-//   var server = meteor();
-//   var client = browser(server);
-//
-//   beforeEach(function () {
-//     server.execute(function () {
-//
-//     }).then(function (value){
-//
-//     });
-//   });
-//   afterEach(function () {
-//     server.execute(function () {
-//
-//     });
-//   });
-//
-//   it('StatsCounter should exist on the client', function () {
-//     return client.execute(function () {
-//       expect(StatsCounter).to.exist;
-//     });
-//   });
-//
-//   it('StatsCounter should exist on the server', function () {
-//     return server.execute(function () {
-//       expect(StatsCounter).to.exist;
-//     });
-//   });
-// });
+
+describe('clinical:graphs-dailystats', function () {
+  var server = meteor();
+  var client = browser(server);
+
+  it('StatsCounter should exist on the client', function () {
+    return client.execute(function () {
+      expect(StatsCounter).to.exist;
+    });
+  });
+
+  it('StatsCounter should exist on the server', function () {
+    return server.execute(function () {
+      expect(StatsCounter).to.exist;
+    });
+  });
+
+  it('Graphs should exist on the client', function () {
+    return client.execute(function () {
+      expect(Graphs).to.exist;
+    });
+  });
+
+  it('Graphs should exist on the server', function () {
+    return server.execute(function () {
+      expect(Graphs).to.exist;
+    });
+  });
+});
